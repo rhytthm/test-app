3a419a9ffb4893990a0181b2e2d208e9d3070721
Rhytthm <rhytthm1717@gmail.com>
2025-04-14 10:43:19 +0530

    . e | point test step to SwiftUI-TDD working directory
    #-----| Arlo's Commit Notation cheat sheet:
    # * f | Feature (> 8 LoC)
    # - f | Feature (<= 8 LoC)
    # * b | Bug fix (> 8 LoC)
    # - b | Bug fix (<= 8 LoC)
    # * r | Non-provable refactoring
    # - r | Test-supported procedural refactoring
    # . r | Automated refactoring
    # . d | Developer-visible documentation
    # . a | Automatic formatting/generation
    # . e | Environment (non-code) changes that affect development setup
    # . t | Test-only

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -e  # Exit on first error
 
 SCHEME='SwiftUI-TDD'
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
 
-xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
+echo "🏗️ Running tests in: $(pwd)"
+echo "🔍 Looking for .xcodeproj files:"
+find . -name "*.xcodeproj"
+
+xcodebuild test \
+  -scheme "$SCHEME" \
+  -sdk iphonesimulator \
+  -destination "$DESTINATION" \
+  CODE_SIGNING_ALLOWED=NO
