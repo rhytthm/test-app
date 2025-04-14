3ff5a6176122fb428de70267b564d9e65735cecd
Rhytthm <rhytthm1717@gmail.com>
2025-04-14 06:26:17 +0530

    e - make a test plan

---
+++ Test-AppTests/Test_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Test_AppTests.swift
+//  Test-AppTests
+//
+//  Created by RAJEEV MAHAJAN on 14/04/25.
+//
+
+import Testing
+
+struct Test_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
