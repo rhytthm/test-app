1bc59404aac83ae65cd235160a6e9735f249989b
Rhytthm <rhytthm1717@gmail.com>
2025-04-14 08:05:03 +0530

    . t | Adding own custom test file

---
+++ Test-AppTests/AppTests.swift
@@ -0,0 +1,9 @@
+@testable import Test_App
+import XCTest
+
+@MainActor
+final class AppTests: XCTestCase, Sendable {
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in AppTests")
+    }
+}
