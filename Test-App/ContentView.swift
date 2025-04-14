6f05de61f6f95ecd30008767c12ede96ec297a48
Rhytthm <rhytthm1717@gmail.com>
2025-04-14 06:12:42 +0530

    e - New Xcode project Interface: SwiftUI, Language: swift

---
+++ Test-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test-App
+//
+//  Created by RAJEEV MAHAJAN on 14/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test-App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test-App
+//
+//  Created by RAJEEV MAHAJAN on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
